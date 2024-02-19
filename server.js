747ea2d00b7922d1dc79b8cabccbda746888e1e4
Ayush9535 <ghodkeayush22@gmail.com>
2024-02-19 11:51:36 +0530

    Codium Updates Made 2

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express")
-
+const port = process.env.PORT
 const app = express()
 
 app.get("/ping", (req, res) => {
-    res.send("pong")
+    try {
+        res.send("pong")
+    } catch (err) {
+        console.log(err)
+    }
 })
 
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.log(err)
     } else {
